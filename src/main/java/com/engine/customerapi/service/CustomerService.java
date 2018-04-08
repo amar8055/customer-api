e3bcc337e6027ab7a14c2d82df95ea70f68539f9
'Amar <amarjnit7097@gmail.com>
2018-04-08 16:03:57 -0500

    Four Commit

--- src/main/java/com/engine/customerapi/service/CustomerService.java
+++ src/main/java/com/engine/customerapi/service/CustomerService.java
@@ -13,60 +13,53 @@ public class CustomerService {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-	//FindAll
-	public List<Customer> getAll(){
-		
+
+	// FindAll
+	public List<Customer> getAll() {
+
 		return customerRepository.findAll();
-		
+
 	}
-	
-	//Find One 
-	public Customer getById(int id){
-		
+
+	// Find One
+	public Customer getById(int id) {
+
 		return customerRepository.findOne(id);
 	}
 
-	
-	//Create
+	// Create
 	public Customer create(Customer customer) {
-		
+
 		return customerRepository.save(customer);
 	}
 
-	
-	//Update
+	// Update
 	public Customer update(int id, Customer customer) throws Exception {
-		
+
 		Customer customerdb = this.getById(id);
-		
-		if(customerdb == null) {
-			
-			throw new Exception("You Did a terrible Mistake" +id + "does not exist");
-			
+
+		if (customerdb == null) {
+
+			throw new Exception("You Did a terrible Mistake" + id + "does not exist");
+
 		}
-		
+
 		return customerRepository.save(customerdb);
 	}
-	
-	
-	//Delete
+
+	// Delete
 	public void delete(int id) throws Exception {
-		
+
 		Customer customerDelete = this.getById(id);
 
-		if(customerDelete == null) {
-			
-			throw new Exception("You Did a terrible Mistake" +id + "does not exist");
-			
+		if (customerDelete == null) {
+
+			throw new Exception("You Did a terrible Mistake" + id + "does not exist");
+
 		}
-		 
-			customerRepository.delete(id);
-		
-		
+
+		customerRepository.delete(id);
+
 	}
-	
-	
-	
-	
+
 }
